6e8f54cdc797516f880b12db08cab3618919c3e1
matt roche <matt.roche@gmail.com>
2023-03-26 11:44:00 -0500

    complete phase 0 javascript events acting on events lab

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const dodger = document.getElementById("dodger");
 
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+
+dodger.style.left = "180px";
+
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
-
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
@@ -18,8 +23,7 @@ document.addEventListener("keydown", function (e) {
 function moveDodgerRight() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
-
-  if (left > 0) {
+  if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
 }
